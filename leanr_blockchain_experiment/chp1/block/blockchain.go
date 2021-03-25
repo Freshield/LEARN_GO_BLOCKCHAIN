2a286925bfc0494b36dace017bc1ef69079ff567
freshield <yangyufresh@163.com>
2021-03-25 20:38:23 +0800

    done blockchain

---
+++ leanr_blockchain_experiment/chp1/block/blockchain.go
@@ -0,0 +1,38 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: blockchain.py
+@Time: 2021-12-08 16:24
+@Last_update: 2021-12-08 16:24
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package block
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func (bc *Blockchain) GetBlocks() []*Block {
+	return bc.blocks
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
