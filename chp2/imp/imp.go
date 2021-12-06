5b708c35f80dec7c65fcbfa3086cef7bed60fad9
freshield <yangyufresh@163.com>
2021-12-06 15:48:30 +0800

    imp

---
+++ chp2/imp/imp.go
@@ -0,0 +1,22 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: imp.py
+@Time: 2021-12-01 22:11
+@Last_update: 2021-12-01 22:11
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package imp
+
+import "fmt"
+
+func Show(name string, age int) {
+	fmt.Printf("My Name is %s, My age is %d\n", name, age)
+}
