ae0e18b615507d7fbc62b59d764a7afecc6cf9d2
freshield <yangyufresh@163.com>
2022-01-07 21:27:05 +0800

    add wallet

---
+++ learn_blockchain_experiment/chp5/block/wallet.go
@@ -0,0 +1,94 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: wallet.py
+@Time: 2022-01-14 20:17
+@Last_update: 2022-01-14 20:17
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package block
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"golang.org/x/crypto/ripemd160"
+	"log"
+)
+
+const version = byte(0x00)
+const walletFile = "wallet.dat"
+const addressChecksumLen = 4
+
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey []byte
+}
+
+type Wallets struct {
+	Wallets map[string]*Wallet
+}
+
+func NewWallet() *Wallet {
+	private, public := newKeyPair()
+	wallet := Wallet{private, public}
+
+	return &wallet
+}
+
+// 新建私钥公钥
+func newKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+	pubKey := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
+
+	return *private, pubKey
+}
+
+// 获取地址
+func (w Wallet) GetAddress() []byte {
+	// hash pubkey
+	pubKeyHash := HashPubKey(w.PublicKey)
+	// 合并version和公钥的hash，算出checksum
+	versionedPayload := append([]byte{version}, pubKeyHash...)
+	checksum := checksum(versionedPayload)
+
+	// 生成最终的地址
+	fullPayload := append(versionedPayload, checksum...)
+	address := Base58Encode(fullPayload)
+
+	return address
+}
+
+// 对公钥hash
+func HashPubKey(pubKey []byte) []byte {
+	publicSHA256 := sha256.Sum256(pubKey)
+
+	RIPEMD160Hasher := ripemd160.New()
+	_, err := RIPEMD160Hasher.Write(publicSHA256[:])
+	if err != nil {
+		log.Panic(err)
+	}
+	publicRIPEMD160 := RIPEMD160Hasher.Sum(nil)
+
+	return publicRIPEMD160
+}
+
+// 获得公钥的checksum
+func checksum(payload []byte) []byte {
+	firstSHA := sha256.Sum256(payload)
+	secondSHA := sha256.Sum256(firstSHA[:])
+
+	return secondSHA[:addressChecksumLen]
+}
