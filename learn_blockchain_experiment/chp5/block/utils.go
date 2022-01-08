ecd24f4ed5d4583b8371a30b0e5fa659c19f1dab
freshield <yangyufresh@163.com>
2022-01-08 21:26:54 +0800

    add reverse bytes

--- learn_blockchain_experiment/chp5/block/utils.go
+++ learn_blockchain_experiment/chp5/block/utils.go
@@ -29,3 +29,10 @@ func IntToHex(num int64) []byte {
 	}
 	return buff.Bytes()
 }
+
+// 把字节颠倒
+func ReverseBytes(data []byte) {
+	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
+		data[i], data[j] = data[j], data[i]
+	}
+}
